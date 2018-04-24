ddb2f37f83cce167e72fd0ba999143031d94b910
Sebastian Hidalgo <sgha10@hotmail.com>
2018-04-24 00:29:54 -0600

    adding the preview image, visible color change in the bodywork of the car, fixing some logic

--- public/src/app/car_builder/color-picker/color-picker.component.ts
+++ public/src/app/car_builder/color-picker/color-picker.component.ts
@@ -12,16 +12,40 @@ export class ColorPickerComponent implements OnInit {
   @Output() colorChanged = new EventEmitter();
 
   colors = [
-    {id: 'black', hexcode: '#010101'},
-    {id: 'red', hexcode: '#E50101'}, 
-    {id: 'white', hexcode: '#FEFEFE'}, 
-    {id: 'blue', hexcode: '#1301B5'}, 
-    {id: 'yellow', hexcode: '#F7FE32'}
+    { id: 'black', hexcode: '#010101' },
+    { id: 'red', hexcode: '#E50101' },
+    { id: 'white', hexcode: '#FEFEFE' },
+    { id: 'blue', hexcode: '#1301B5' },
+    { id: 'yellow', hexcode: '#F7FE32' }
   ];
 
   onColorChange(colorId) {
     this.selectedColor = colorId;
     this.colorChanged.emit(this.selectedColor);
+
+    switch (colorId) {
+      case 'black':
+        document.getElementById("bodyImage").style.filter = "opacity(.5) drop-shadow(0 0 0 black)";
+        console.log("negro");
+        break;
+      case 'red':
+        document.getElementById("bodyImage").style.filter = "opacity(.5) drop-shadow(0 0 0 red)";
+        console.log("red");
+        break;
+      case 'white':
+        document.getElementById("bodyImage").style.filter = "opacity(.5) drop-shadow(0 0 0 white)";
+        console.log("white");
+        break;
+      case 'blue':
+        document.getElementById("bodyImage").style.filter = "opacity(.5) drop-shadow(0 0 0 blue)";
+        console.log("blue");
+        break;
+      case 'yellow':
+        document.getElementById("bodyImage").style.filter = "opacity(.5) drop-shadow(0 0 0 yellow)";
+        console.log("yellow");
+        break;
+    }
+
   }
 
   constructor() { }
