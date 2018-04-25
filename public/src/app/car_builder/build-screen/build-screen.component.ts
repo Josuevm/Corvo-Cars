143f99ca479f8d637c99714a0f15f7dd49d2ba6b
Sebastian Hidalgo <sgha10@hotmail.com>
2018-04-25 00:18:43 -0600

    real time change of cars in the builder, color changes for all models, fixing some logic

--- public/src/app/car_builder/build-screen/build-screen.component.ts
+++ public/src/app/car_builder/build-screen/build-screen.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import {
+  DomSanitizer, SafeHtml,
+  SafeUrl,
+  SafeStyle
+} from '@angular/platform-browser';
 
 @Component({
   selector: 'build-screen',
@@ -6,33 +12,68 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./build-screen.component.css']
 })
 export class BuildScreenComponent implements OnInit {
-  
+
   @Input('buildOption') buildOption;
   @Input('model') model;
 
   car = {
-    modelID: -1,
-    colorID : ''
+    modelID: 0,
+    colorID: ''
   };
 
-  constructor() { }
+  
+
+  //esto despues se cambia ya con la logica de las images desde la base 
+  carsImages = [
+    [
+      "../../../assets/images/Imparatus/SUV-1.png",
+      "../../../assets/images/Imparatus/SUV-2.png",
+      "../../../assets/images/Imparatus/SUV-3.png"
+    ]
+    ,
+    [
+      "../../../assets/images/Kubanyi/RAM-1.png",
+      "../../../assets/images/Kubanyi/RAM-2.png",
+      "../../../assets/images/Kubanyi/RAM-3.png"
+    ]
+    ,
+    [
+      "../../../assets/images/Imperiale/Imperiale-1.png",
+      "../../../assets/images/Imperiale/Imperiale-2.png",
+      "../../../assets/images/Imperiale/Imperiale-3.png"
+    ]
+  ]
+    ;
+
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(){
+   //cambia el id, de una vez en cadena se cambia lo grafico
+    this.car.modelID = this.model;
+  }
+
+  //recibe el tipo de imagen que se ocupa, se encarga de devolverla la parte adecuada del carro seleccionado 
+  getBackground(part) {
+    return this.carsImages[this.car.modelID][part];
   }
 
   showPreview() {
     this.car.modelID = this.model;
     alert('modelID: ' + this.car.modelID + ' colorID: ' + this.car.colorID);
- }
+  }
 
- setColor(color) {
+  setColor(color) {
     this.car.colorID = color;
     this.changeCarColor();
   }
 
   changeCarColor() {
-      //images logic goes HERE
-  } 
+    //images logic goes HERE
+  }
 
 
 }
