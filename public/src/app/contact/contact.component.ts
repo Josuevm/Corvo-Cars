b38838c21fb6440cd8f2b18560e0c754cc642ce2
unknown <josuevm37@hotmail.com>
2018-06-08 19:58:05 -0600

    Contact form with validations

--- public/src/app/contact/contact.component.ts
+++ public/src/app/contact/contact.component.ts
@@ -14,6 +14,10 @@ export class ContactComponent implements OnInit {
     body: ""
   }
 
+  nameIsIncorrect = false;
+  emailIsIncorrect = false;
+  messageIsIncorrect = false;
+
   showMessage : boolean = false;
   alert :String = "";
 
@@ -24,24 +28,68 @@ export class ContactComponent implements OnInit {
   }
 
   sendMessage(){
-    if(this.message.name.length !== 0 ||
-      this.message.email.length !== 0 ||
-      this.message.body.length !== 0){
-
-        
-        this.alert = "Thank you for contacting us!"
-        this.http.post('/contact_email/',this.message).subscribe(res=>{});
-        this.showMessage = true;
-
-    }else{
-      this.alert = "You need to fill all the information";
-      this.showMessage = true;
+    this.nameValidator();
+    this.emailValidator();
+    this.messageValidator();
+    if(!this.nameIsIncorrect &&
+      !this.emailIsIncorrect &&
+      !this.messageIsIncorrect){
+          this.alert = "Thank you for contacting us!"
+          this.http.post('/contact_email/',this.message).subscribe(res=>{});
+          this.showMessage = true;
     }
     
   }
 
   hideAlert(){
     this.showMessage = false;
+  }
+
+  
+
+  emailValidator(){
+    let emailPattern = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$");
+    let valid = true;
+    this.emailIsIncorrect = false;
+    if(this.message.email.length === 0){
+      this.alert = "You need to fill all the information";
+      this.showMessage = true;
+      this.emailIsIncorrect = true;
+      valid = false;
+    }else if(!emailPattern.test(this.message.email)){
+      this.alert = "E-mail format is incorrect";
+      this.showMessage = true;
+      this.emailIsIncorrect = true;
+      valid = false;
+    }
+  }
+
+  nameValidator(){
+    let valid = true;
+    let namePattern = new RegExp("^[a-zA-Z ]+$");
+    this.nameIsIncorrect = false;
+    if(this.message.name.length === 0){
+      this.alert = "You need to fill all the information";
+      this.showMessage = true;
+      this.nameIsIncorrect = true;
+      valid = false;
+    }else if(!namePattern.test(this.message.name)){
+      this.alert = "Name must contain only letters";
+      this.showMessage = true;
+      this.nameIsIncorrect = true;
+      valid = false;
+    }
+  }
+
+  messageValidator(){
+    let valid = true;
+    this.messageIsIncorrect = false;
+    if(this.message.body.length === 0){
+      this.alert = "You need to fill all the information";
+      this.showMessage = true;
+      this.messageIsIncorrect = true;
+      valid = false;
+    }
   }
 
   changeNavStyle(){
--- routes/contact_email.js
+++ routes/contact_email.js
@@ -3,12 +3,8 @@ var router = express.Router();
 var Contact_email = require('../models/Contact_email.js')
 
 router.post('/', function (req,res,next){
-    console.log(req.body)
     Contact_email(req.body.name,req.body.email,req.body.body);
-    console.log("entra a router");
     res.json("['message':'Email send']")
 })
-
-//Añadir a index o sino no sirve
 
 module.exports = router;
