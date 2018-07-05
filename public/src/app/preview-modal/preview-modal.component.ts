d651988aebe3869ee5f75ac16c57c69badd9f5c3
Sebastian Hidalgo <sgha10@hotmail.com>
2018-07-05 03:44:56 -0600

    fixing responsive css ipad, ipad pro, pc; adding shit pdf because chinox caqueo xddd

--- public/src/app/preview-modal/preview-modal.component.ts
+++ public/src/app/preview-modal/preview-modal.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Output, ViewChild,ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 import { BsModalRef, BsModalService, ModalDirective } from 'ngx-bootstrap/modal';
 import { SelectedCarService } from '../selected-car.service';
 import * as jsPDF from 'jspdf';
 import * as html2canvas from 'html2canvas';
+
 import { Router } from '@angular/router';
 
 @Component({
@@ -15,114 +16,143 @@ export class PreviewModalComponent implements OnInit {
 
   bsModalRef: BsModalRef;
   subT = 0;
-  specs : any;
+  specs: any;
   generatingPDF: boolean = false;
 
-  details =[]
+  details = []
 
-  constructor(private modalService: BsModalService, 
+  constructor(private modalService: BsModalService,
     private selectedCarSrv: SelectedCarService,
     private router: Router) { }
 
 
   ngOnInit() {
-    this.subT=0;
+    this.subT = 0;
     this.selectedCarSrv.specs.subscribe(res => {
       this.specs = res;
+      this.calculateTotal();
       this.getDetails();
-      
+
+
     });
-    this.calculateTotal();
+
 
   }
 
 
-  getDetails(){
-    
- 
-    this.details =[
+  getDetails() {
+
+
+    this.details = [
       {
         name: 'Inside',
         desc: this.specs.inside.name,
-        price:this.specs.inside.price,
+        price: this.specs.inside.price,
         icon: 'drive_eta'
 
       },
       {
         name: 'Rims',
         desc: this.specs.rims.name,
-        price:this.specs.rims.price,
+        price: this.specs.rims.price,
         icon: 'brightness_high'
+      },
+      {
+        name: 'Color',
+        desc: this.specs.color.name,
+        price: "0",
+        icon: 'color_lens'
       },
       {
         name: 'Motor',
         desc: this.specs.motor.name,
-        price:this.specs.motor.price,
+        price: this.specs.motor.price,
         icon: 'build'
       },
       {
         name: 'Extras',
         desc: this.getExtras(),
-        price:this.getExtraP1(),
+        price: this.getExtraP1(),
+        icon: 'playlist_add'
+      },
+      {
+        name: 'Total',
+        price: this.subT,
         icon: 'playlist_add'
       },
     ]
   }
 
-  getExtras(){
+  getExtras() {
     let extras = [];
-    for(let extra of this.specs.extras){
+    for (let extra of this.specs.extras) {
       extras.push(extra.name)
 
     }
     return extras;
   }
-getExtraP(){
-  for(let extra of this.specs.extras){
-    this.subT+=parseInt(extra.price);
+  getExtraP() {
+    for (let extra of this.specs.extras) {
+      this.subT += parseInt(extra.price);
+    }
   }
-}
 
-getExtraP1(){
-  //Imparatus
-  let subT = 0
+  getExtraP1() {
+    //Imparatus
+    let subT = 0
 
-  for(let extra of this.specs.extras){
-    subT+=parseInt(extra.price);
+    for (let extra of this.specs.extras) {
+      subT += parseInt(extra.price);
+    }
+
+    return subT
   }
-  
-  return subT
-}
 
 
-  calculateTotal(){ 
+  calculateTotal() {
     this.getExtraP()
     if (this.selectedCarSrv.getModelID() == 1) {
-      this.subT+= 22000  
+      this.subT += 22000
       //imperiale
-      } else if(this.selectedCarSrv.getModelID() == 2){
+    } else if (this.selectedCarSrv.getModelID() == 2) {
       this.subT += 250000
-      }else{
-      this.subT+=34000
-      }
-    this.subT+=parseInt(this.specs.motor.price) +parseInt(this.specs.inside.price)+parseInt(this.specs.rims.price);
-    
+    } else {
+      this.subT += 34000
+    }
+    this.subT += parseInt(this.specs.motor.price) + parseInt(this.specs.inside.price) + parseInt(this.specs.rims.price);
+
   }
 
   downloadPDF() {
-    let element = document.getElementById("PDFcontent");
-    let divPDF = document.createElement("div");
-    divPDF.style.width = "100%";
-    divPDF.style.height = "100%";
-    divPDF.style.backgroundColor = "black";
-    divPDF.appendChild(element);
-    html2canvas(element).then(function(canvas) {
-        // Export the canvas to its data URI representation
-        let base64image = canvas.toDataURL();
-        let pdf = new jsPDF();
-        pdf.addImage(base64image, 45, 10, 110, 90);
-        pdf.save('CorvoCar.pdf');
+
+    let clone = $("#PDFcontent").clone();
+    let cloneCar = $("#car").clone();
+    clone.css({
+      height: 2000,
+      "font-size": "2vw",
+      "color": "white",
+      "text-transform": "uppercase",
+      "font-family": 'Rajdhani, sans-serif'
+
+    }).appendTo("#containerPDF");
+
+    cloneCar.css({
+      width: 350,
+      "position": "absolute",
+      "margin-top": "-80%",
+      "left": "15%"
+    }).appendTo("#containerPDF");
+
+    let pdfC = $("#containerPDF");
+
+    var pdf = new jsPDF();
+
+    pdf.addHTML(pdfC, function () {
+      pdf.save('web.pdf');
+      clone.remove();
+      cloneCar.remove();
     });
+
   }
 
 
